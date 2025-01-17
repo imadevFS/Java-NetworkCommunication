b9efe33d48ff286575b47ad37875d3f5ed73de86
ImaDev <154860874+kodebidean@users.noreply.github.com>
2025-01-17 19:00:53 +0100

    Update Ejercicio01.java

--- src/Ejercicio01.java
+++ src/Ejercicio01.java
@@ -4,17 +4,18 @@ import java.net.UnknownHostException;
 public class Ejercicio01 {
     public static void main(String[] args) {
         try {
-            // REVISAR EJERCICIO 01
+            InetAddress localHost = InetAddress.getLocalHost();
+
             // Método que devuelve un objeto InetAdress conteniendo nombre y dirección IP del host local
-            System.out.println("Host: " + InetAddress.getLocalHost()); // Nombre y IP
-            System.out.println("Nombre: " + InetAddress.getLocalHost().getHostName()); // Solo el nombre
-            System.out.println("IP: " + InetAddress.getLocalHost().getHostAddress()); // Solo la IP
+            System.out.println("Host: " + localHost); // Nombre y IP
+            System.out.println("Nombre: " + localHost.getHostName()); // Solo el nombre
+            System.out.println("IP: " + localHost.getHostAddress()); // Solo la IP
 
             // Obtener el objeto InetAddress de un host específico
             InetAddress host = InetAddress.getByName("www.ejemplo.com"); // Cambia "www.ejemplo.com" por el host que desees
             System.out.println("Nombre del host específico: " + host.getHostName());
             System.out.println("IP del host específico: " + host.getHostAddress());
-
+            
         } catch (UnknownHostException e) {
             System.out.println("Error: Host no encontrado.");
         }
